c1247e5c16bb402480dee08d0310eac6c9ffb487
Avradeep Nayak <91881492+Zedoman@users.noreply.github.com>
2023-12-27 12:31:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const apiKey = "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"; //hugging face token
-
+//Avradeep Nayak
 const imageCount = 4;   // how many images we want
 let selectImageNumber = null;
-
+//Avradeep Nayak
 // to generate random number between min and max
 function getRandomNum(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,21 +10,21 @@ function getRandomNum(min, max){
 
 function disableGenerateButton(){
     document.getElementById("generate").disable = true;
-}
+}//Avradeep Nayak
 
 function enableGenerateButton(){
     document.getElementById("generate").disable = false;
-}
+}//Avradeep Nayak
 
 function clearImageGrid(){   // to clear the images after each generate button click...
     const imageGrid = document.getElementById("image-grid");
     imageGrid.innerHTML = "";
-}
+}//Avradeep Nayak
 
 
 
 // generate images
-async function generateImages(input){
+async function generateImages(input){//Avradeep Nayak
     disableGenerateButton();
     clearImageGrid();
 
@@ -39,7 +39,7 @@ async function generateImages(input){
         const prompt = `${input} ${randomNum}`;
         const response = await fetch(
             "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
-            {
+            {//Avradeep Nayak
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -50,7 +50,7 @@ async function generateImages(input){
         )
         if(!response.ok){
             alert("Unable to generate images...");
-        }
+        }//Avradeep Nayak
 
         const result = await response.blob();  // blob is to get the response as promise
         const imageUrl = URL.createObjectURL(result);  // URL returns a newly created url which was given by response
@@ -62,23 +62,23 @@ async function generateImages(input){
         img.alt = `art-${i + 1}`;
         img.onclick = () => downloadImage(imageUrl, i);
         document.getElementById("image-grid").appendChild(img);
-    }
+    }//Avradeep Nayak
     loading.style.display = "none";
     enableGenerateButton();
-    
+    //Avradeep Nayak
     selectImageNumber = null;
 }
-
+//Avradeep Nayak
 
 function downloadImage(imageUrl, imageNumbers){
     const link = document.createElement("a");
     link.href = imageUrl;
     link.download = `image-${imageNumbers+1}.png`;
     link.click();
-}
+}//Avradeep Nayak
 
 // event listener for generate button
 document.getElementById("generate").addEventListener('click', ()=>{
     const input = document.getElementById("user-prompt").value;
     generateImages(input);
-})
+})//Avradeep Nayak
